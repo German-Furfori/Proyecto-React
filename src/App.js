f7c2e7f4186659ebb165a8135a923c4846e4f042
German <german.furfori@gmail.com>
2022-08-02 11:57:33 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Characters from './Components/Characters';
+import ResponsiveAppBar from './Components/ResponsiveAppBar';
 
 function App() {
   return (
     <div>
+      <ResponsiveAppBar/>
       <Characters/>
     </div>
   );
